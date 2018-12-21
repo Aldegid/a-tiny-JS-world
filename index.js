57adcff1fd76cd82f3e57e009370423dddc0ec66
Aldegid <aldegid@gmail.com>
2018-12-21 16:14:03 +0200

    add my solution for tiny world

--- index.js
+++ index.js
@@ -7,6 +7,61 @@
 
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
+function outCreature(obj) {
+  var msg = [];
+  for (let key of Object.keys(obj)) {
+    msg.push(obj[key]);
+  }  
+  return  `Hello i am a <strong>${msg[0]}</strong>. My name is <strong>${msg[1]}</strong> and i am a ${msg[2]}. I have <strong>${msg[3]}</strong> legs and <strong>${msg[4]}</strong> hands and i saying: <em>${msg[5]}</em><hr>`;  
+}
+
+const dog = {
+  species: 'dog',
+  name: 'Toby',
+  gender: 'male',
+  legs: 4,
+  hands: 0,
+  saying: 'woof-woof!'
+};
+
+const cat = {
+  species: 'cat',
+  name: 'Murka',
+  gender: 'female',
+  legs: 4,
+  hands: 0,
+  saying: 'meow-meow!'
+};
+
+const man = {
+  species: 'human',
+  name: 'Vasiliy',
+  gender: 'male',
+  legs: 2,
+  hands: 2,
+  saying: 'I am hungry!'
+};
+
+const woman = {
+  species: 'human',
+  name: 'Marusiya',
+  gender: 'female',
+  legs: 2,
+  hands: 2,
+  saying: 'listen to me!'
+};
+
+const catWoman = {
+  species: cat.species + "-" + woman.species,
+  name: "Selina Kyle",
+  gender: "female",
+  legs: 2,
+  hands: 2,
+  saying: woman.saying + " and sometimes " + cat.saying 
+};
+
+
+
 
 
 // ======== OUTPUT ========
@@ -17,6 +72,11 @@
    However, please, REFRAIN from improving visuals at least until your code is reviewed
    so code reviewers might focus on a single file that is index.js.
    */
+  print(outCreature(dog));
+  print(outCreature(cat));
+  print(outCreature(man));
+  print(outCreature(woman));
+  print(outCreature(catWoman));
 
 /* Print examples:
    print('ABC');
