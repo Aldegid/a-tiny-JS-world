72d5485e81bde7a84345a69e523cf804f81fc995
Aldegid <aldegid@gmail.com>
2018-12-26 21:33:41 +0200

    build a-tiny-JS-world with OOP

--- index.js
+++ index.js
@@ -1,91 +1,64 @@
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
-
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
-   */
-
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
-function outCreature(obj) {
-  var msg = [];
-  for (let key of Object.keys(obj)) {
-    msg.push(obj[key]);
-  }  
-  return  `Hello i am a <strong>${msg[0]}</strong>. My name is <strong>${msg[1]}</strong> and i am a ${msg[2]}. I have <strong>${msg[3]}</strong> legs and <strong>${msg[4]}</strong> hands and i saying: <em>${msg[5]}</em><hr>`;  
-}
-
-const dog = {
-  species: 'dog',
-  name: 'Toby',
-  gender: 'male',
-  legs: 4,
-  hands: 0,
-  saying: 'woof-woof!'
+let Inhabitant = function(gender, name, saying){
+  this.gender = gender;
+  this.name = name;
+  this.saying = saying;
 };
 
-const cat = {
-  species: 'cat',
-  name: 'Murka',
-  gender: 'female',
-  legs: 4,
-  hands: 0,
-  saying: 'meow-meow!'
+Inhabitant.prototype.makeMessage = function(){
+  return `Hello i am a <strong>${this.species}</strong>. My name is <strong>${this.name}</strong> and i am a ${this.gender}.I have <strong>${this.legs}</strong> legs and <strong>${this.hands}</strong> hands and i saying: <em>${this.saying}</em><hr>`;
 };
 
-const man = {
-  species: 'human',
-  name: 'Vasiliy',
-  gender: 'male',
-  legs: 2,
-  hands: 2,
-  saying: 'I am hungry!'
+let Dog = function(gender, name, saying){
+  Inhabitant.call(this, gender, name, saying);
+  this.species = 'dog';
+  this.legs = 4;
+  this.hands = 0;
 };
 
-const woman = {
-  species: 'human',
-  name: 'Marusiya',
-  gender: 'female',
-  legs: 2,
-  hands: 2,
-  saying: 'listen to me!'
+Dog.prototype = Object.create(Inhabitant.prototype);
+
+let Cat = function(gender, name, saying){
+  Inhabitant.call(this, gender, name, saying);
+  this.species = 'cat';
+  this.saying = 'meow-meow!';
+  this.legs = 4;
+  this.hands = 0;
 };
 
-const catWoman = {
-  species: cat.species + "-" + woman.species,
-  name: "Selina Kyle",
-  gender: "female",
-  legs: 2,
-  hands: 2,
-  saying: woman.saying + " and sometimes " + cat.saying 
+Cat.prototype = Object.create(Inhabitant.prototype);
+
+let Human = function(gender, name, saying){
+  Inhabitant.call(this, gender, name, saying);
+  this.species = 'human';
+  this.legs = 2;
+  this.hands = 2;
 };
 
+Human.prototype = Object.create(Inhabitant.prototype);
 
+let Cathuman = function(gender, name){
+  Inhabitant.call(this, gender, name, cat, woman);
+  this.species = 'cat-human';
+  this.legs = 2;
+  this.hands = 2;
+  this.cat = cat;
+  this.woman = woman;
+  this.saying = this.sayMutate();
+};
 
+Cathuman.prototype = Object.create(Inhabitant.prototype);
+Cathuman.prototype.sayMutate = function(){
+  return `${this.woman.saying} and sometimes ${this.cat.saying}`;
+};
 
+let dog = new Dog('male', 'Toby', 'woof-woof!');
+let cat = new Cat('female', 'Murka', 'meow-meow!');
+let man = new Human('male', 'Vasiliy', 'I am hungry!');
+let woman = new Human('female', 'Marusiya', 'Listen to me!');
+let catwoman = new Cathuman('female', 'Selina Kyle');
 
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-  print(outCreature(dog));
-  print(outCreature(cat));
-  print(outCreature(man));
-  print(outCreature(woman));
-  print(outCreature(catWoman));
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
-
-
+print(dog.makeMessage());
+print(cat.makeMessage());
+print(man.makeMessage());
+print(woman.makeMessage());
+print(catwoman.makeMessage());
